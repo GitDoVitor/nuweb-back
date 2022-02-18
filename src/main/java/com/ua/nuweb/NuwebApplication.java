4699615c025b641cb0f95cd89c8e54f83c71db69
gitdovitor <joaovitorsantoscs@gmail.com>
2022-02-17 21:36:00 -0300

    feat(ALL): Initial Commit

---
+++ src/main/java/com/ua/nuweb/NuwebApplication.java
@@ -0,0 +1,13 @@
+package com.ua.nuweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ua/nuweb/NuwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.nuweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
